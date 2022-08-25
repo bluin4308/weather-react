a6fc7cb2a00fba60c8955063cf045057c102dddf
Volodymyr U <ukraincev.vova@gmail.com>
2022-08-25 18:59:31 +0300

    fix image import

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-
+import photo from "../../assets/6nMLIKd.png";
 export default class About extends React.Component {
   componentDidMount() {
     this.props.updateTitle("About");
   }
   render() {
-    return (
-      <img className="portrait" src="/src/assets/6nMLIKd.png" alt="Oleg" w />
-    );
+    return <img className="portrait" src={photo} alt="Oleg" />;
   }
 }
