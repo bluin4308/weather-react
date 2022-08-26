6abf28907bdc8aa90ef5a32ff7d008e30ac42691
Volodymyr U <ukraincev.vova@gmail.com>
2022-08-26 15:00:30 +0300

    try to fix fetch error in prod

--- src/services/fetch-wrapper.js
+++ src/services/fetch-wrapper.js
@@ -1,6 +1,7 @@
 export default async function fetchWrapper(location) {
   const response = await fetch(
-    `http://localhost:4000/weather?address=${location}`
+    // `http://localhost:4000/weather?address=${location}`
+    `/weather?address=${location}`
   );
   const json = await response.json();
   return json;
