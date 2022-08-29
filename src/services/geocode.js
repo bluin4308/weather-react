49c6b94579a1467f8a1ad0bdaafe7fe4f7be9ab5
Volodymyr U <ukraincev.vova@gmail.com>
2022-08-30 01:07:38 +0300

    geocode.js&weather.js rewrite using async/await

--- src/services/geocode.js
+++ src/services/geocode.js
@@ -1,26 +1,45 @@
 const fetch = require("node-fetch");
-const geocode = (address, callback) => {
+const geocode = async (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
   )}.json?access_token=pk.eyJ1Ijoib3RqdSIsImEiOiJjbDcyZThtdjkwamx5NDFuOTF2aW1oMDhuIn0.EZ1ULM7-jCIBhir_F3qkXw&limit=1`;
-  fetch(url)
-    .then((response) => response.json())
-    .then(
-      (json) => {
-        if (json.features.length === 0)
-          callback("Unable to find location. Try another search!");
-        else {
-          console.log(json);
-          const geolocation = json.features[0];
-          const latitude = geolocation.center[1];
-          const longitude = geolocation.center[0];
-          const location = geolocation.place_name;
-          callback(undefined, { latitude, longitude, location });
-        }
-      },
-      (error) => {
-        callback(error);
-      }
-    );
+  const response = await fetch(url);
+  try {
+    const json = await response.json();
+    if (json.features.length === 0) {
+      callback("Unable to find location. Try another search!");
+    } else {
+      const geolocation = json.features[0];
+      const latitude = geolocation.center[1];
+      const longitude = geolocation.center[0];
+      const location = geolocation.place_name;
+      callback(undefined, { latitude, longitude, location });
+    }
+  } catch (error) {
+    callback(error);
+  }
 };
 module.exports = { geocode };
+// const geocode = (address, callback) => {
+//   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+//     address
+//   )}.json?access_token=pk.eyJ1Ijoib3RqdSIsImEiOiJjbDcyZThtdjkwamx5NDFuOTF2aW1oMDhuIn0.EZ1ULM7-jCIBhir_F3qkXw&limit=1`;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then(
+//       (json) => {
+//         if (json.features.length === 0)
+//           callback("Unable to find location. Try another search!");
+//         else {
+//           const geolocation = json.features[0];
+//           const latitude = geolocation.center[1];
+//           const longitude = geolocation.center[0];
+//           const location = geolocation.place_name;
+//           callback(undefined, { latitude, longitude, location });
+//         }
+//       },
+//       (error) => {
+//         callback(error);
+//       }
+//     );
+// };
--- src/services/weather.js
+++ src/services/weather.js
@@ -1,24 +1,44 @@
 const fetch = require("node-fetch");
-const weather = (latitude, longitude, callback) => {
+const weather = async (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=190559d6c071c1cbe6ffcf68868a3328&query=${latitude},${longitude}&units=m`;
-  fetch(url)
-    .then((response) => response.json())
-    .then(
-      (json) => {
-        if (json.error) callback("Weather ERROR!");
-        else {
-          const weather = json.current;
-          callback(undefined, {
-            temperature: weather.temperature,
-            description: weather.weather_descriptions[0],
-            feelslike: weather.feelslike,
-            region: json.location.region,
-          });
-        }
-      },
-      (error) => {
-        callback(error);
-      }
-    );
+  const response = await fetch(url);
+  const json = await response.json();
+  try {
+    if (json.error) callback("Weather ERROR!");
+    else {
+      const weather = json.current;
+      callback(undefined, {
+        temperature: weather.temperature,
+        description: weather.weather_descriptions[0],
+        feelslike: weather.feelslike,
+        region: json.location.region,
+      });
+    }
+  } catch (error) {
+    callback(error);
+  }
 };
 module.exports = { weather };
+
+// const weather = (latitude, longitude, callback) => {
+//   const url = `http://api.weatherstack.com/current?access_key=190559d6c071c1cbe6ffcf68868a3328&query=${latitude},${longitude}&units=m`;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then(
+//       (json) => {
+//         if (json.error) callback("Weather ERROR!");
+//         else {
+//           const weather = json.current;
+//           callback(undefined, {
+//             temperature: weather.temperature,
+//             description: weather.weather_descriptions[0],
+//             feelslike: weather.feelslike,
+//             region: json.location.region,
+//           });
+//         }
+//       },
+//       (error) => {
+//         callback(error);
+//       }
+//     );
+// };
